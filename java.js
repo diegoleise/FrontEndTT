ea11314cd12252209aeedca071992a96adf3e5ed
diegoleise <diegoleise@gmail.com>
2024-11-20 14:03:13 -0300

    agrego js

--- java.js
+++ java.js
@@ -1,1 +1,16 @@
-console.log(2 + "3");
+console.log(2 + 3);
+let productos = 4
+console.log(productos)
+console.log("la suma de 2 + 2 es: " + (2+2))
+
+let nombreUsuario = "Diego"
+let correoUsuario = "diegoleise@gmail"
+
+console.log("el nombre del usuario es: " + nombreUsuario)
+console.log(correoUsuario)
+
+console.log("Suma de numeros")
+var numero1 = 20
+var numero2 = 30
+var sumaNumeros = numero1 + numero2
+console.log("La suma de los numeros es: " + sumaNumeros)
