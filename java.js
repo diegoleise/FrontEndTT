222183905845eb35ddec7d1638afaee63628071c
diegoleise <diegoleise@gmail.com>
2024-11-22 19:17:41 -0300

    cambio imagenes y colores

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-console.log(2 + 3);
+/*console.log(2 + 3);
 let productos = 4
 console.log(productos)
 console.log("la suma de 2 + 2 es: " + (2+2))
@@ -14,3 +14,21 @@ var numero1 = 20
 var numero2 = 30
 var sumaNumeros = numero1 + numero2
 console.log("La suma de los numeros es: " + sumaNumeros)
+
+var vidas = 3
+const DESCONTAR = 1
+vidas = vidas - DESCONTAR
+console.log(vidas)
+
+let nombreUser = prompt("ingrese un dato")
+let apellidoUser = prompt("ingrese un apellido")
+nombreUser = Number(nombreUser)
+apellidoUser = Number(apellidoUser)
+let sumaPrompt = nombreUser + apellidoUser
+console.log(sumaPrompt)
+console.log(typeof nombreUser)
+console.log(typeof apellidoUser)*/
+
+let edad1 = prompt ("ingrese su edad")
+document.write(edad1)
+console.log(edad1)
