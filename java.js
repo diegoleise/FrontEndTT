898bdb052df432cd5ba5c478970790e43405e038
diegoleise <diegoleise@gmail.com>
2024-11-27 22:30:24 -0300

    cambio js curso

--- java.js
+++ java.js
@@ -18,17 +18,64 @@ console.log("La suma de los numeros es: " + sumaNumeros)
 var vidas = 3
 const DESCONTAR = 1
 vidas = vidas - DESCONTAR
-console.log(vidas)
+console.log(vidas)+*/
 
-let nombreUser = prompt("ingrese un dato")
-let apellidoUser = prompt("ingrese un apellido")
-nombreUser = Number(nombreUser)
-apellidoUser = Number(apellidoUser)
-let sumaPrompt = nombreUser + apellidoUser
-console.log(sumaPrompt)
-console.log(typeof nombreUser)
-console.log(typeof apellidoUser)*/
 
-let edad1 = prompt ("ingrese su edad")
+/*let edad1 = prompt ("ingrese su edad")
 document.write(edad1)
-console.log(edad1)
+console.log(edad1)*/
+//prompt es para obtener datos del usuario
+//document.write es para escribir en la pagina
+//console.log es para escribir en la consola del navegador
+//prompt devuelve un string, por lo que debemos convertirlo a number para realizar operaciones
+
+/*let dia = parseInt(prompt("ingrese dia de la semana"))
+if (dia == 1) {
+    alert("es lunes")
+}
+else if (dia == 2) {
+    alert("es martes")
+}
+else if (dia == 3) {
+    alert("es miercoles")
+}
+else {
+    alert("no es lunes, martes o miercoles")
+}
+parseInt convierte un string a un numero entero
+switch(dia) {
+    case 1:
+        alert("es lunes")
+        break
+    case 2:
+        alert("es martes")
+        break
+    case 3:
+        alert("es miercoles")
+    default:
+        alert("no es lunes, martes o miercoles")
+}
+
+let saldo = 0
+let opcion = parseInt(prompt("ingrese una opcion entre 1 y 4"))
+
+switch(opcion){
+    case 1:
+        alert("verificar saldo "+ saldo)
+        if (saldo<=0){
+            alert("saldo insuficiente")
+        }
+        break
+         }
+let argentina = 1
+while (argentina<5){
+   // let nombre1 = prompt("ingrese su nombre")
+    alert("soy un ciclo "+ argentina)
+    argentina++
+    
+}*/
+
+for(var i = 0; i<5; i++){
+    
+    console.log("probando ciclo for "+i)
+}
